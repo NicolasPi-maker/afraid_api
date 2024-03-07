d7ba24272c0581532601acd8efd8032b1754def1
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-03-07 19:21:45 +0100

    feature/step_02:migration execution

--- database/migrations/2024_03_07_175603_create_roles_table.php
+++ database/migrations/2024_03_07_175603_create_roles_table.php
@@ -17,13 +17,6 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->timestamps();
         });
-
-        Schema::table('users', function (Blueprint $table) {
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('cascade');
-        });
     }
 
     /**
